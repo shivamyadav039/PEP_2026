2f02b6627b3dd177d5279d5fc30eeafb3c473807
shivamyadav <shivamyadav7745@gmail.com>
2026-01-31 13:08:58 +0530

    updated

--- CPP/mergesort.cpp
+++ CPP/mergesort.cpp
@@ -1,33 +1,45 @@
 #include <iostream>
 using namespace std;
-int ppartition(int arr[],int start;int end){
+
+int partition(int arr[], int start, int end) {
     int pivot = arr[start];
-    int count=0;
-    for(int i=start;i<end;i++){
-        if(arr[i]<=pivot) count++;
+    int count = 0;
+
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
     }
-    int pivotindex = start+count;
-    swap(arr[pivotindex],arr[start]);
-    int i= start,j=end;
-    while(i<pivotindex && j>=pivotindex){
-        while(arr[i]<pivot)i++;
-        while(arr[j]>pivot)j++;
-        swap(arr[i],arr[j]);
+
+    int pivotindex = start + count;
+    swap(arr[pivotindex], arr[start]);
+
+    int i = start, j = end;
+
+    while (i < pivotindex && j > pivotindex) {
+        while (arr[i] <= pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i < pivotindex && j > pivotindex)
+            swap(arr[i], arr[j]);
     }
+
     return pivotindex;
-    
 }
-void quicksort(int arr[],int start,int end){
-    if(start >=end)return;
-    int p = partition(arr,start,end);
-    quicksort(arr,start,p-1);
-    quicksort(arr,p+1,end);
-    
+
+void quicksort(int arr[], int start, int end) {
+    if (start >= end) return;
+
+    int p = partition(arr, start, end);
+    quicksort(arr, start, p - 1);
+    quicksort(arr, p + 1, end);
 }
-int main(){
-    int arr[] = {2,5,6,9,10,12,7};
-    quicksort(arr,0,6);
-    for(int i:arr){
-        cout<<i<<" ";
-    }
+
+int main() {
+    int arr[] = {2, 5, 6, 9, 10, 12, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quicksort(arr, 0, n - 1);
+
+    for (int i : arr)
+        cout << i << " ";
 }
