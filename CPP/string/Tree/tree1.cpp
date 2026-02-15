0d449ad1b2392d6d58c9514c5ee0c38d3a7aea24
shivamyadav <shivamyadav7745@gmail.com>
2026-02-15 13:01:23 +0530

    update

---
+++ CPP/string/Tree/tree1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+    void inorder(Node* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+
+    int count(Node* root){
+        if(root == NULL) return 0;
+        int x = count(root->left);
+        int y = count(root->right);
+        return x+y+1;
+    }
+    int countleafnode(Node* root){
+        if(root==NULL || root->left == NULL && root->right) return 1;
+        
+        int y = countleafnode(root->right);
+        
+    }
+};
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    root->inorder(root);
+    cout<<endl;
+
+    int res = root->count(root);
+    cout<<res;
+}
