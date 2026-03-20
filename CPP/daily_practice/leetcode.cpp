b281bdd4e8dfcaf3703eab63e193096fdae6387e
shivamyadav <shivamyadav7745@gmail.com>
2026-03-20 20:53:16 +0530

    update

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -262,3 +262,47 @@ auto init = []()
     cout.tie(0);
     return 'c';
 }();
+
+
+https://leetcode.com/problems/minimum-absolute-difference-in-sliding-submatrix/description/?envType=daily-question&envId=2026-03-20
+
+
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> ans(n - k + 1, vector<int>(m - k + 1));
+        for (int i = 0; i <= n - k; i++) {
+            map<int, int> mp;
+            for (int r = i; r < i + k; r++) {
+                for (int c = 0; c < k; c++) {
+                    mp[grid[r][c]]++;
+                }
+            }
+            for (int c = 0; c <= m - k; c++) {
+                if (mp.size() > 1) {
+                    int mini = 1e9;
+                    auto it = mp.begin();
+                    int prev = it->first;
+                    for (++it; it != mp.end(); it++) {
+                        mini = min(mini, abs(it->first - prev));
+                        prev = it->first;
+                    }
+                    ans[i][c] = mini;
+                } else {
+                    ans[i][c] = 0;
+                }
+
+                if (c < m - k) {
+                    for (int r = i; r < i + k; r++) {
+                        mp[grid[r][c]]--;
+                        if (mp[grid[r][c]] == 0)
+                            mp.erase(grid[r][c]);
+                        mp[grid[r][c + k]]++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
