8521c4eb08e0f9332b5cd582fc6e4cb409737b9e
shivamyadav <shivamyadav7745@gmail.com>
2026-03-27 22:11:15 +0530

    update

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -484,3 +484,31 @@ public:
         return solve(mat);
     }
 };
+
+https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/description/?envType=daily-question&envId=2026-03-27
+
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        k %= n;  //(reduce k<n)
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 0) {
+                    // even row , left rotate
+                    if (mat[i][j] != mat[i][(j + k) % n])
+                        return false;
+                } else {
+                    // odd row , right rotate
+                    if (mat[i][j] != mat[i][(j - k + n) % n])
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
