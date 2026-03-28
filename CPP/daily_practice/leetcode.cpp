6e46608400c47594e6f625c58650a1eee3c096e0
shivamyadav <shivamyadav7745@gmail.com>
2026-03-28 22:14:54 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -512,3 +512,51 @@ public:
         return true;
     }
 };
+
+https://leetcode.com/problems/find-the-string-with-lcp/description/?envType=daily-question&envId=2026-03-28
+
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        string word(n, '\0');
+        char current = 'a';
+
+        for (int i = 0; i < n; i++) {
+            if (word[i] == '\0') {
+                if (current > 'z') {
+                    return "";
+                }
+                word[i] = current;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        word[j] = word[i];
+                    }
+                }
+                current++;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (word[i] != word[j]) {
+                    if (lcp[i][j]) {
+                        return "";
+                    }
+                } else {
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) {
+                            return "";
+                        }
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
+                            return "";
+                        }
+                    }
+                }
+            }
+        }
+
+        return word;
+    }
+};
