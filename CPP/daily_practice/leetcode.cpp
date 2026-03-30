e5352ea8f0c0b2e07dd87a0426f9817a26a16629
shivamyadav <shivamyadav7745@gmail.com>
2026-03-30 21:06:47 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -579,3 +579,30 @@ public:
         return s1 == s2;
     }
 };
+
+
+https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/description/?envType=daily-question&envId=2026-03-30
+
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        vector<int> even(26, 0), odd(26, 0);
+
+        for(int i = 0; i < s1.size(); i++) {
+            if(i % 2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(even[i] != 0 || odd[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
