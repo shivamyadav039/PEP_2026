cb22c69381f7e9d266031fe91587cbd03ec980e7
shivamyadav <shivamyadav7745@gmail.com>
2026-04-05 21:06:55 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -863,3 +863,19 @@ public:
         return ans;
     }
 };
+
+https://leetcode.com/problems/robot-return-to-origin/description/?envType=daily-question&envId=2026-04-05
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (char m : moves) {
+            if (m == 'R') x++;
+            else if (m == 'L') x--;
+            else if (m == 'U') y++;
+            else if (m == 'D') y--;
+        }
+        return x == 0 && y == 0;
+    }
+};
