4197275d386ca36af13e19e543298a090dc322d2
shivamyadav <shivamyadav7745@gmail.com>
2026-04-08 21:57:18 +0530

    update

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -879,3 +879,28 @@ public:
         return x == 0 && y == 0;
     }
 };
+
+
+https://leetcode.com/problems/xor-after-range-multiplication-queries-i/description/?envType=daily-question&envId=2026-04-08
+
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        const int MOD = 1e9 + 7;
+        
+        for (int i=0;i<queries.size();i++) {
+            int l = queries[i][0], r = queries[i][1], k = queries[i][2], v = queries[i][3];
+            
+            for (int idx = l; idx <= r; idx += k) {
+                nums[idx] = (1LL * nums[idx] * v) % MOD;
+            }
+        }
+        
+        int ans = 0;
+        for (int i=0;i<nums.size();i++) {
+            ans ^= nums[i];
+        }
+        
+        return ans;
+    }
+};
