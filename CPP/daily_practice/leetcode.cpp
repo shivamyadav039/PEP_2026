e46a642baba2eabaca927281d4771396a3cb40aa
shivamyadav <shivamyadav7745@gmail.com>
2026-04-15 21:00:07 +0530

    update

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -955,3 +955,18 @@ public:
         
     }
 };
+
+https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/?envType=daily-question&envId=2026-04-15
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int start) {
+        int n = words.size();
+        for (int i = 0; i <= n >> 1; i++)
+            if (words[(start + i) % n] == target |
+                words[(start - i + n) % n] == target)
+                return i;
+                
+        return -1;
+    }
+};
