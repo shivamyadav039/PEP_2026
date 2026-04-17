a81340ce32c41816caab9522233f85c8b65d4fca
shivamyadav <shivamyadav7745@gmail.com>
2026-04-17 22:08:27 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1014,3 +1014,34 @@ public:
     }
 };
 
+https://leetcode.com/problems/minimum-absolute-distance-between-mirror-pairs/description/?envType=daily-question&envId=2026-04-17
+
+
+ class Solution {
+public:
+    int reverse(int val) {
+        int newnum = 0;
+        while (val != 0) {
+            int x = val % 10;
+            val /= 10;
+            newnum = newnum * 10 + x;
+        }
+        return newnum;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int mindist = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+
+            if (mp.find(nums[i]) != mp.end()) {
+                mindist = min(mindist, i - mp[nums[i]]);
+            }
+            int rev = reverse(nums[i]);
+            mp[rev] = i;
+        }
+        return mindist == INT_MAX ? -1 : mindist;
+    }
+};
