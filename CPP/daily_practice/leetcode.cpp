0d1ade941b4679fefdd90f96146fb04b236f1ed9
shivamyadav <shivamyadav7745@gmail.com>
2026-04-19 16:24:37 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1063,3 +1063,26 @@ public:
         
     }
 };
+
+https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/description/?envType=daily-question&envId=2026-04-19
+
+
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0;
+        int ans = 0;
+
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] <= nums2[j]){
+                ans = max(ans, j - i);
+                j++;
+            }else{
+                i++;
+                if(i > j) j = i;
+            }
+        }
+
+        return ans;
+    }
+};
