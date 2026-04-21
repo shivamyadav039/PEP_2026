2a75c7296a390a04554df7c70469d62f0505c1de
shivamyadav <shivamyadav7745@gmail.com>
2026-04-21 20:57:08 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1086,3 +1086,46 @@ public:
         return ans;
     }
 };
+
+https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/description/?envType=daily-question&envId=2026-04-21
+
+class Solution {
+public:
+    vector<int> parent;
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int a, int b) {
+        parent[find(a)] = find(b);
+    }
+
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+
+        for (auto& swap : allowedSwaps)
+            unite(swap[0], swap[1]);
+
+        // Group source values by their root
+        unordered_map<int, unordered_map<int, int>> groupFreq;
+        for (int i = 0; i < n; i++)
+            groupFreq[find(i)][source[i]]++;
+
+        int hammingDistance = 0;
+        for (int i = 0; i < n; i++) {
+            int root = find(i);
+            auto& freq = groupFreq[root];
+            if (freq.count(target[i]) && freq[target[i]] > 0)
+                freq[target[i]]--;
+            else
+                hammingDistance++;
+        }
+
+        return hammingDistance;
+    }
+};
