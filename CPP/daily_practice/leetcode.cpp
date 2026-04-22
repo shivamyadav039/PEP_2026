6f42c0888546543774d61861cb1337e10a587f54
shivamyadav <shivamyadav7745@gmail.com>
2026-04-22 21:06:40 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1129,3 +1129,30 @@ public:
         return hammingDistance;
     }
 };
+
+https://leetcode.com/problems/words-within-two-edits-of-dictionary/description/?envType=daily-question&envId=2026-04-22
+
+
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries[0].size();
+        vector<string> ans;
+        for(auto &it:queries){
+            for(auto &i:dictionary){
+                int diff=0;
+                for(int idx=0; idx<n; idx++){
+                    if(it[idx]!=i[idx]){
+                        diff++;
+                    }
+                    if(diff>2) break;
+                }
+                if(diff<=2){
+                    ans.push_back(it);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
