2512c4ba6db07dd8d9a83af88b82cf506a5ac82b
shivamyadav <shivamyadav7745@gmail.com>
2026-05-11 20:53:29 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1514,4 +1514,20 @@ public:
     }
 };
 
-asdfghjkfg
+https://leetcode.com/problems/separate-the-digits-in-an-array/description/?envType=daily-question&envId=2026-05-11
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int val = nums[i];
+            while (val > 0) {
+                ans.push_back(val % 10);
+                val /= 10;
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
