ce581d51e70cd492ffde12fca904476c89e6e2ae
shivamyadav <shivamyadav7745@gmail.com>
2026-05-21 22:02:45 +0530

    leetcode

--- CPP/daily_practice/leetcode.cpp
+++ CPP/daily_practice/leetcode.cpp
@@ -1531,3 +1531,56 @@ public:
         return ans;
     }
 };
+
+https://leetcode.com/problems/minimum-common-value/description/?envType=daily-question&envId=2026-05-19
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int result=-1;
+        int n1=nums1.size(),n2=nums2.size();
+        int i=0,j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]==nums2[j]){
+                result=nums1[i];
+                break;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return result;
+    }
+};
+
+https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/?envType=daily-question&envId=2026-05-21
+
+
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<string>st;
+        for(int i=0;i<arr1.size();i++){
+            string s=to_string(arr1[i]);
+            for(int i=0;i<=s.size();i++){
+                st.insert(s.substr(0,i));
+            }
+        }
+        int ans=0;
+        for(int i=0;i<arr2.size();i++){
+            string s=to_string(arr2[i]);
+            for(int i=0;i<=s.size();i++){
+                string x=s.substr(0,i);
+                if(st.count(x)){
+                    int y=x.size();
+                    ans=max(ans,y);
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
